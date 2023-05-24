df6610833d7a9d0d288867730fb0e0b162e13c12
jorgen.kodehode <jorgen.kodehode@gmail.com>
2023-05-24 11:06:33 +0200

    changed App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const oauthToken = ""; // oauth token goes here.
-
+const APIData = ""; // API Data goes here.
 const client = new Discord.Client();
 
 client.on("message", (msg) => {
